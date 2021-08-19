c3111509b586464c4180b60e48084b0deda3d2f5
David Langdale <David.Langdale@global.ntt>
2021-08-19 17:00:34 +1000

    Test

--- ssg/extensions.py
+++ ssg/extensions.py
@@ -1,5 +1,9 @@
-import sys,importlib
+import sys
+import importlib
 from pathlib import Path
+
 
 def load_module(directory, name):
     sys.path.insert(0, directory)
+    importlib.import_module(name)
+
